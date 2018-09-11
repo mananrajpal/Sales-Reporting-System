188ecf95253d7c27dd5cf47201aeb0997874e4a5
Manan Rajpal <100864824@student.swin.edu.au>
2018-09-11 18:02:21 +1000

    Created the basic UI which includes text fields and label

---
+++ phpsreps/AddProduct.Designer.cs
@@ -0,0 +1,243 @@
+﻿namespace phpsreps
+{
+    partial class AddProduct
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.product_code = new System.Windows.Forms.TextBox();
+            this.category_code = new System.Windows.Forms.TextBox();
+            this.product_name = new System.Windows.Forms.TextBox();
+            this.company_id = new System.Windows.Forms.TextBox();
+            this.cost_price = new System.Windows.Forms.TextBox();
+            this.retail_price = new System.Windows.Forms.TextBox();
+            this.in_stock = new System.Windows.Forms.TextBox();
+            this.submit = new System.Windows.Forms.Button();
+            this.clear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(229, 72);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(111, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Product Code";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(229, 113);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(120, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Category Code";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(229, 154);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(116, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Product Name";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(229, 197);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(97, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Company Id";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(229, 245);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(88, 20);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Cost Price";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(229, 289);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(96, 20);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Retail Price";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(229, 337);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(69, 20);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "In Stock";
+            // 
+            // product_code
+            // 
+            this.product_code.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.product_code.Location = new System.Drawing.Point(390, 66);
+            this.product_code.MaxLength = 5;
+            this.product_code.Name = "product_code";
+            this.product_code.Size = new System.Drawing.Size(185, 26);
+            this.product_code.TabIndex = 7;
+            // 
+            // category_code
+            // 
+            this.category_code.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.category_code.Location = new System.Drawing.Point(390, 107);
+            this.category_code.Name = "category_code";
+            this.category_code.Size = new System.Drawing.Size(185, 26);
+            this.category_code.TabIndex = 8;
+            // 
+            // product_name
+            // 
+            this.product_name.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.product_name.Location = new System.Drawing.Point(390, 148);
+            this.product_name.Name = "product_name";
+            this.product_name.Size = new System.Drawing.Size(185, 26);
+            this.product_name.TabIndex = 9;
+            // 
+            // company_id
+            // 
+            this.company_id.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.company_id.Location = new System.Drawing.Point(390, 191);
+            this.company_id.Name = "company_id";
+            this.company_id.Size = new System.Drawing.Size(185, 26);
+            this.company_id.TabIndex = 10;
+            // 
+            // cost_price
+            // 
+            this.cost_price.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cost_price.Location = new System.Drawing.Point(390, 239);
+            this.cost_price.Name = "cost_price";
+            this.cost_price.Size = new System.Drawing.Size(185, 26);
+            this.cost_price.TabIndex = 11;
+            // 
+            // retail_price
+            // 
+            this.retail_price.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.retail_price.Location = new System.Drawing.Point(390, 283);
+            this.retail_price.Name = "retail_price";
+            this.retail_price.Size = new System.Drawing.Size(185, 26);
+            this.retail_price.TabIndex = 12;
+            // 
+            // in_stock
+            // 
+            this.in_stock.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.in_stock.Location = new System.Drawing.Point(390, 334);
+            this.in_stock.Name = "in_stock";
+            this.in_stock.Size = new System.Drawing.Size(185, 26);
+            this.in_stock.TabIndex = 13;
+            // 
+            // submit
+            // 
+            this.submit.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.submit.Location = new System.Drawing.Point(335, 410);
+            this.submit.Name = "submit";
+            this.submit.Size = new System.Drawing.Size(120, 42);
+            this.submit.TabIndex = 14;
+            this.submit.Text = "Submit";
+            this.submit.UseVisualStyleBackColor = true;
+            // 
+            // clear
+            // 
+            this.clear.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.clear.Location = new System.Drawing.Point(487, 410);
+            this.clear.Name = "clear";
+            this.clear.Size = new System.Drawing.Size(107, 42);
+            this.clear.TabIndex = 15;
+            this.clear.Text = "Clear";
+            this.clear.UseVisualStyleBackColor = true;
+            // 
+            // AddProduct
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(919, 512);
+            this.Controls.Add(this.clear);
+            this.Controls.Add(this.submit);
+            this.Controls.Add(this.in_stock);
+            this.Controls.Add(this.retail_price);
+            this.Controls.Add(this.cost_price);
+            this.Controls.Add(this.company_id);
+            this.Controls.Add(this.product_name);
+            this.Controls.Add(this.category_code);
+            this.Controls.Add(this.product_code);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "AddProduct";
+            this.Text = "AddProduct";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox product_code;
+        private System.Windows.Forms.TextBox category_code;
+        private System.Windows.Forms.TextBox product_name;
+        private System.Windows.Forms.TextBox company_id;
+        private System.Windows.Forms.TextBox cost_price;
+        private System.Windows.Forms.TextBox retail_price;
+        private System.Windows.Forms.TextBox in_stock;
+        private System.Windows.Forms.Button submit;
+        private System.Windows.Forms.Button clear;
+    }
+}
