759d469ce497980a470df8a6bcb0c9343841fce6
Manan Rajpal <100864824@student.swin.edu.au>
2018-09-16 19:55:18 +1000

    Implemented basic validation of input fields
    At the moment it checks for empty fields and if the prices are in decimal and the stock to be in positive number

--- phpsreps/AddProduct.cs
+++ phpsreps/AddProduct.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Text.RegularExpressions;
 
 namespace phpsreps
 {
@@ -15,14 +16,15 @@ namespace phpsreps
         private String product_code, category_code, product_name, company_id;
         private double cost_price, retail_price;
         private int in_stock;
-        private string _errors;
-        private ErrorProvider errorProvider;
+        private ErrorProvider errorprovider;
+        private String _regexfordecimal, _regexforstock;
 
         public AddProduct()
         {
             InitializeComponent();
-            errorProvider = new ErrorProvider();
-            
+            errorprovider = new ErrorProvider();
+            _regexfordecimal = @"^\d*\.+\d*$";
+            _regexforstock = @"^[1-9]\d*$";
         }
 
         private void clear_Click(object sender, EventArgs e)
@@ -52,37 +54,55 @@ namespace phpsreps
             if (product_codeTextBox.Text.Length == 0)
             {
                 product_codeTextBox.Focus();
-                errorProvider.SetError(product_codeTextBox, MessageBox.Show("Please enter the Product Code", "Error",MessageBoxButtons.OK, MessageBoxIcon.Error).ToString());
+                errorprovider.SetError(product_codeTextBox, MessageBox.Show("Please enter the Product Code", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error).ToString());
                 return false;
             }
             else if (category_codeTextBox.Text.Length == 0)
             {
                 category_codeTextBox.Focus();
-                errorProvider.SetError(category_codeTextBox, MessageBox.Show("Please enter the Category Code", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error).ToString());
+                errorprovider.SetError(category_codeTextBox, MessageBox.Show("Please enter the Category Code", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error).ToString());
                 return false;
             }
             else if (company_idTextBox.Text.Length == 0)
             {
                 company_idTextBox.Focus();
-                errorProvider.SetError(company_idTextBox, MessageBox.Show("Please enter the Company ID", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error).ToString());
+                errorprovider.SetError(company_idTextBox, MessageBox.Show("Please enter the Company ID", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error).ToString());
                 return false;
             }
             else if (cost_priceTextBox.Text.Length == 0)
             {
                 cost_priceTextBox.Focus();
-                errorProvider.SetError(cost_priceTextBox, MessageBox.Show("Please enter the Cost Price", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error).ToString());
+                errorprovider.SetError(cost_priceTextBox, MessageBox.Show("Please enter the Cost Price", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error).ToString());
                 return false;
             }
             else if (retail_priceTextBox.Text.Length == 0)
             {
                 retail_priceTextBox.Focus();
-                errorProvider.SetError(retail_priceTextBox, MessageBox.Show("Please enter the Retail Price", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error).ToString());
+                errorprovider.SetError(retail_priceTextBox, MessageBox.Show("Please enter the Retail Price", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error).ToString());
                 return false;
             }
             else if (in_stockTextBox.Text.Length == 0)
             {
                 in_stockTextBox.Focus();
-                errorProvider.SetError(in_stockTextBox, MessageBox.Show("Please enter the Stock Count", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error).ToString());
+                errorprovider.SetError(in_stockTextBox, MessageBox.Show("Please enter the Stock Count", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error).ToString());
+                return false;
+            }
+            else if (!Regex.IsMatch(cost_priceTextBox.Text, _regexfordecimal))
+            {
+                cost_priceTextBox.Focus();
+                errorprovider.SetError(cost_priceTextBox, MessageBox.Show("Please enter the Cost Price in Decimal Format", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error).ToString());
+                return false;
+            }
+            else if (!Regex.IsMatch(retail_priceTextBox.Text, _regexfordecimal))
+            {
+                retail_priceTextBox.Focus();
+                errorprovider.SetError(retail_priceTextBox, MessageBox.Show("Please enter the Retail Price in Decimal Format", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error).ToString());
+                return false;
+            }
+            else if (!Regex.IsMatch(in_stockTextBox.Text, _regexforstock))
+            {
+                in_stockTextBox.Focus();
+                errorprovider.SetError(in_stockTextBox, MessageBox.Show("Stock Count can only be positive", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error).ToString());
                 return false;
             }
             else
@@ -96,12 +116,12 @@ namespace phpsreps
             //check the validation first
             if (validation() == true)
             {
-                errorProvider.Clear();
+                errorprovider.Clear();
                 InitializeVariable();
             }
             else
             {
-                errorProvider.Clear();
+                errorprovider.Clear();
             }
             //if everything is right than proceed to connecting with database
             //submit the details to the server
