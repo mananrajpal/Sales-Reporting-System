5908b13f999b733b94cb79e6353024d0a2cc9e9f
Ruby Mae Orfiano <101344657@student.swin.edu.au>
2018-10-10 19:47:23 +1100

    Edit specific product form

---
+++ phpsreps/editProduct.Designer.cs
@@ -0,0 +1,264 @@
+﻿namespace phpsreps
+{
+	partial class editProduct
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label8 = new System.Windows.Forms.Label();
+			this.product_code = new System.Windows.Forms.TextBox();
+			this.inStock = new System.Windows.Forms.TextBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.label5 = new System.Windows.Forms.Label();
+			this.label6 = new System.Windows.Forms.Label();
+			this.label7 = new System.Windows.Forms.Label();
+			this.category = new System.Windows.Forms.TextBox();
+			this.product_name = new System.Windows.Forms.TextBox();
+			this.company_id = new System.Windows.Forms.TextBox();
+			this.cost_price = new System.Windows.Forms.TextBox();
+			this.retail_price = new System.Windows.Forms.TextBox();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Kristen ITC", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.Location = new System.Drawing.Point(187, 37);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(91, 23);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Edit item";
+			this.label1.Click += new System.EventHandler(this.label1_Click);
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Font = new System.Drawing.Font("Bookman Old Style", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label2.Location = new System.Drawing.Point(58, 98);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(100, 16);
+			this.label2.TabIndex = 1;
+			this.label2.Text = "Product code:";
+			this.label2.Click += new System.EventHandler(this.label2_Click);
+			// 
+			// label8
+			// 
+			this.label8.AutoSize = true;
+			this.label8.Font = new System.Drawing.Font("Bookman Old Style", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label8.Location = new System.Drawing.Point(58, 353);
+			this.label8.Name = "label8";
+			this.label8.Size = new System.Drawing.Size(67, 16);
+			this.label8.TabIndex = 7;
+			this.label8.Text = "In-stock:";
+			// 
+			// product_code
+			// 
+			this.product_code.BackColor = System.Drawing.SystemColors.Info;
+			this.product_code.Location = new System.Drawing.Point(191, 97);
+			this.product_code.Name = "product_code";
+			this.product_code.Size = new System.Drawing.Size(197, 20);
+			this.product_code.TabIndex = 8;
+			// 
+			// inStock
+			// 
+			this.inStock.BackColor = System.Drawing.SystemColors.Info;
+			this.inStock.Location = new System.Drawing.Point(191, 352);
+			this.inStock.Name = "inStock";
+			this.inStock.Size = new System.Drawing.Size(117, 20);
+			this.inStock.TabIndex = 14;
+			// 
+			// button1
+			// 
+			this.button1.BackColor = System.Drawing.SystemColors.ButtonFace;
+			this.button1.Font = new System.Drawing.Font("Kristen ITC", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button1.Location = new System.Drawing.Point(101, 431);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(95, 30);
+			this.button1.TabIndex = 15;
+			this.button1.Text = "Update";
+			this.button1.UseVisualStyleBackColor = false;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// button2
+			// 
+			this.button2.BackColor = System.Drawing.SystemColors.ButtonFace;
+			this.button2.Font = new System.Drawing.Font("Kristen ITC", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button2.Location = new System.Drawing.Point(253, 431);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(95, 30);
+			this.button2.TabIndex = 16;
+			this.button2.Text = "Close";
+			this.button2.UseVisualStyleBackColor = false;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Font = new System.Drawing.Font("Bookman Old Style", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label3.Location = new System.Drawing.Point(58, 143);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(74, 16);
+			this.label3.TabIndex = 17;
+			this.label3.Text = "Category:";
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Font = new System.Drawing.Font("Bookman Old Style", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label4.Location = new System.Drawing.Point(58, 181);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(107, 16);
+			this.label4.TabIndex = 18;
+			this.label4.Text = "Product name:";
+			// 
+			// label5
+			// 
+			this.label5.AutoSize = true;
+			this.label5.Font = new System.Drawing.Font("Bookman Old Style", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label5.Location = new System.Drawing.Point(58, 223);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(95, 16);
+			this.label5.TabIndex = 19;
+			this.label5.Text = "Company ID:";
+			// 
+			// label6
+			// 
+			this.label6.AutoSize = true;
+			this.label6.Font = new System.Drawing.Font("Bookman Old Style", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label6.Location = new System.Drawing.Point(58, 267);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(81, 16);
+			this.label6.TabIndex = 20;
+			this.label6.Text = "Cost price:";
+			// 
+			// label7
+			// 
+			this.label7.AutoSize = true;
+			this.label7.Font = new System.Drawing.Font("Bookman Old Style", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label7.Location = new System.Drawing.Point(58, 311);
+			this.label7.Name = "label7";
+			this.label7.Size = new System.Drawing.Size(92, 16);
+			this.label7.TabIndex = 21;
+			this.label7.Text = "Retail price:";
+			// 
+			// category
+			// 
+			this.category.BackColor = System.Drawing.SystemColors.Info;
+			this.category.Location = new System.Drawing.Point(191, 140);
+			this.category.Name = "category";
+			this.category.Size = new System.Drawing.Size(197, 20);
+			this.category.TabIndex = 22;
+			// 
+			// product_name
+			// 
+			this.product_name.BackColor = System.Drawing.SystemColors.Info;
+			this.product_name.Location = new System.Drawing.Point(191, 181);
+			this.product_name.Name = "product_name";
+			this.product_name.Size = new System.Drawing.Size(197, 20);
+			this.product_name.TabIndex = 23;
+			// 
+			// company_id
+			// 
+			this.company_id.BackColor = System.Drawing.SystemColors.Info;
+			this.company_id.Location = new System.Drawing.Point(191, 222);
+			this.company_id.Name = "company_id";
+			this.company_id.Size = new System.Drawing.Size(197, 20);
+			this.company_id.TabIndex = 24;
+			// 
+			// cost_price
+			// 
+			this.cost_price.BackColor = System.Drawing.SystemColors.Info;
+			this.cost_price.Location = new System.Drawing.Point(191, 266);
+			this.cost_price.Name = "cost_price";
+			this.cost_price.Size = new System.Drawing.Size(117, 20);
+			this.cost_price.TabIndex = 25;
+			// 
+			// retail_price
+			// 
+			this.retail_price.BackColor = System.Drawing.SystemColors.Info;
+			this.retail_price.Location = new System.Drawing.Point(191, 310);
+			this.retail_price.Name = "retail_price";
+			this.retail_price.Size = new System.Drawing.Size(117, 20);
+			this.retail_price.TabIndex = 26;
+			// 
+			// editProduct
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+			this.ClientSize = new System.Drawing.Size(464, 496);
+			this.Controls.Add(this.retail_price);
+			this.Controls.Add(this.cost_price);
+			this.Controls.Add(this.company_id);
+			this.Controls.Add(this.product_name);
+			this.Controls.Add(this.category);
+			this.Controls.Add(this.label7);
+			this.Controls.Add(this.label6);
+			this.Controls.Add(this.label5);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.inStock);
+			this.Controls.Add(this.product_code);
+			this.Controls.Add(this.label8);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+			this.Name = "editProduct";
+			this.Text = "PHP-SRePS";
+			this.Load += new System.EventHandler(this.editProduct_Load);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label8;
+		private System.Windows.Forms.TextBox product_code;
+		private System.Windows.Forms.TextBox inStock;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.Label label6;
+		private System.Windows.Forms.Label label7;
+		private System.Windows.Forms.TextBox category;
+		private System.Windows.Forms.TextBox product_name;
+		private System.Windows.Forms.TextBox company_id;
+		private System.Windows.Forms.TextBox cost_price;
+		private System.Windows.Forms.TextBox retail_price;
+	}
+}
---
+++ phpsreps/editProduct.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Text.RegularExpressions;
+
+namespace phpsreps
+{
+	public partial class editProduct : Form
+	{
+		private ErrorProvider errorprovider;
+		private String _regexfordecimal, _regexforstock;
+		 
+
+		public editProduct()
+		{
+			InitializeComponent();
+			errorprovider = new ErrorProvider();
+			_regexfordecimal = @"^\d*\.+\d*$";
+			_regexforstock = @"^[1-9]\d*$";
+		}
+
+		private void label2_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void label3_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void label4_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void label7_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void editProduct_Load(object sender, EventArgs e)
+		{
+
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			if (validation() == true)
+			{
+				string constring = "Data Source=phpsreps.database.windows.net;Initial Catalog=phpsreps;User ID=SwinDP2tu05g1;Password=SwinDP2team;Connect Timeout=30;Encrypt=True;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+				string Query = "update dbo.products set Category='" + this.category.Text + "', Product_Name='" + this.product_name.Text + "', Company_ID='" + this.company_id.Text + "', Cost_Price='" + this.cost_price.Text + "', Retail_Price='" + this.retail_price.Text + "', InStock='" + this.inStock.Text + "' where Product_Code='" + this.product_code.Text + "' ;";
+				SqlConnection conDatabase = new SqlConnection(constring);
+				SqlCommand cmdDatabase = new SqlCommand(Query, conDatabase);
+				SqlDataReader myReader;
+
+				try
+				{
+					conDatabase.Open();
+					myReader = cmdDatabase.ExecuteReader();
+					MessageBox.Show("Updated successfully!");
+					while (myReader.Read())
+					{
+
+					}
+				}
+				catch (Exception ex)
+				{
+					MessageBox.Show(ex.Message);
+				}
+			} else
+			{
+				errorprovider.Clear();
+			}
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			this.Close();
+		}
+
+		private void label1_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private Boolean validation()
+		{
+			if (product_code.Text.Length == 0)
+			{
+				product_code.Focus();
+				errorprovider.SetError(product_code, MessageBox.Show("Please enter the Product Code", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error).ToString());
+				return false;
+			}
+			else if (category.Text.Length == 0)
+			{
+				category.Focus();
+				errorprovider.SetError(category, MessageBox.Show("Please enter the Category		`", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error).ToString());
+				return false;
+			}
+			else if (company_id.Text.Length == 0)
+			{
+				company_id.Focus();
+				errorprovider.SetError(company_id, MessageBox.Show("Please enter the Company ID", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error).ToString());
+				return false;
+			}
+			else if (cost_price.Text.Length == 0)
+			{
+				cost_price.Focus();
+				errorprovider.SetError(cost_price, MessageBox.Show("Please enter the Cost Price", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error).ToString());
+				return false;
+			}
+			else if (retail_price.Text.Length == 0)
+			{
+				retail_price.Focus();
+				errorprovider.SetError(retail_price, MessageBox.Show("Please enter the Retail Price", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error).ToString());
+				return false;
+			}
+			else if (inStock.Text.Length == 0)
+			{
+				inStock.Focus();
+				errorprovider.SetError(inStock, MessageBox.Show("Please enter the Stock Count", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error).ToString());
+				return false;
+			}
+			else if (!Regex.IsMatch(cost_price.Text, _regexfordecimal))
+			{
+				cost_price.Focus();
+				errorprovider.SetError(cost_price, MessageBox.Show("Please enter the Cost Price in Decimal Format", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error).ToString());
+				return false;
+			}
+			else if (!Regex.IsMatch(retail_price.Text, _regexfordecimal))
+			{
+				retail_price.Focus();
+				errorprovider.SetError(retail_price, MessageBox.Show("Please enter the Retail Price in Decimal Format", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error).ToString());
+				return false;
+			}
+			else if (!Regex.IsMatch(inStock.Text, _regexforstock))
+			{
+				inStock.Focus();
+				errorprovider.SetError(inStock, MessageBox.Show("Stock Count can only be positive", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error).ToString());
+				return false;
+			}
+			else if (!(category.Text.Equals(Category.Beauty)) || !(category.Text.Equals(Category.FirstAid)) || !(category.Text.Equals(Category.Medicines)))
+			{
+				category.Focus();
+				errorprovider.SetError(category, MessageBox.Show("Category can only be Beauty, FirstAid or Medicines", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error).ToString());
+				return false;
+			}
+			else if (CheckCompanyCode() == false)
+			{
+				company_id.Focus();
+				errorprovider.SetError(company_id, MessageBox.Show("The Company Id entered does not pre-exist, please enter that company details first or correct the company id", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error).ToString());
+				return false;
+			}
+			else
+			{
+				return true;
+			}
+		}
+
+		private Boolean CheckCompanyCode()
+		{
+			for (int i = 0; i < CompanyList.companies.Count; i++)
+			{
+				if (CompanyList.companies[i].CompanyID.Equals(company_id.Text))
+				{
+					return true;
+				}
+			}
+			return false;
+		}
+
+	}
+}
