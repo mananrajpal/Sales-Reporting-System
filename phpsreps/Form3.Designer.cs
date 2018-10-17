f9ade627f600ad1d38f669f020dcbd2a9dc5f6aa
Ruby Mae Orfiano <101344657@student.swin.edu.au>
2018-10-17 23:28:04 +1100

    fixed code error on Sales screen

--- phpsreps/Form3.Designer.cs
+++ phpsreps/Form3.Designer.cs
@@ -43,8 +43,6 @@
 			this.salesTableAdapter = new phpsreps.phpsrepsDataSet1TableAdapters.salesTableAdapter();
 			this.button1 = new System.Windows.Forms.Button();
 			this.button2 = new System.Windows.Forms.Button();
-			this.button3 = new System.Windows.Forms.Button();
-			this.button4 = new System.Windows.Forms.Button();
 			this.menuStrip1.SuspendLayout();
 			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
 			((System.ComponentModel.ISupportInitialize)(this.salesBindingSource)).BeginInit();
@@ -149,9 +147,9 @@
 			// 
 			this.button1.BackColor = System.Drawing.SystemColors.ButtonFace;
 			this.button1.Font = new System.Drawing.Font("Kristen ITC", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.button1.Location = new System.Drawing.Point(44, 86);
+			this.button1.Location = new System.Drawing.Point(223, 95);
 			this.button1.Name = "button1";
-			this.button1.Size = new System.Drawing.Size(156, 48);
+			this.button1.Size = new System.Drawing.Size(156, 30);
 			this.button1.TabIndex = 3;
 			this.button1.Text = "Sales for a Category";
 			this.button1.UseVisualStyleBackColor = false;
@@ -161,37 +159,13 @@
 			// 
 			this.button2.BackColor = System.Drawing.SystemColors.ButtonFace;
 			this.button2.Font = new System.Drawing.Font("Kristen ITC", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.button2.Location = new System.Drawing.Point(241, 86);
+			this.button2.Location = new System.Drawing.Point(443, 95);
 			this.button2.Name = "button2";
-			this.button2.Size = new System.Drawing.Size(156, 48);
+			this.button2.Size = new System.Drawing.Size(156, 30);
 			this.button2.TabIndex = 4;
 			this.button2.Text = "Sales for a Product";
 			this.button2.UseVisualStyleBackColor = false;
 			this.button2.Click += new System.EventHandler(this.button2_Click);
-			// 
-			// button3
-			// 
-			this.button3.BackColor = System.Drawing.SystemColors.ButtonFace;
-			this.button3.Font = new System.Drawing.Font("Kristen ITC", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.button3.Location = new System.Drawing.Point(437, 86);
-			this.button3.Name = "button3";
-			this.button3.Size = new System.Drawing.Size(156, 48);
-			this.button3.TabIndex = 5;
-			this.button3.Text = "Sales Output for a Category";
-			this.button3.UseVisualStyleBackColor = false;
-			this.button3.Click += new System.EventHandler(this.button3_Click);
-			// 
-			// button4
-			// 
-			this.button4.BackColor = System.Drawing.SystemColors.ButtonFace;
-			this.button4.Font = new System.Drawing.Font("Kristen ITC", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.button4.Location = new System.Drawing.Point(631, 86);
-			this.button4.Name = "button4";
-			this.button4.Size = new System.Drawing.Size(156, 48);
-			this.button4.TabIndex = 6;
-			this.button4.Text = "Sales Output for a Product";
-			this.button4.UseVisualStyleBackColor = false;
-			this.button4.Click += new System.EventHandler(this.button4_Click);
 			// 
 			// Form3
 			// 
@@ -199,8 +173,6 @@
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
 			this.ClientSize = new System.Drawing.Size(833, 511);
-			this.Controls.Add(this.button4);
-			this.Controls.Add(this.button3);
 			this.Controls.Add(this.button2);
 			this.Controls.Add(this.button1);
 			this.Controls.Add(this.dataGridView1);
@@ -237,7 +209,5 @@
 		private System.Windows.Forms.DataGridViewTextBoxColumn saleCostDataGridViewTextBoxColumn;
 		private System.Windows.Forms.Button button1;
 		private System.Windows.Forms.Button button2;
-		private System.Windows.Forms.Button button3;
-		private System.Windows.Forms.Button button4;
 	}
 }
---
+++ phpsreps/SalesForm.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace phpsreps
+{
+	partial class SalesForm
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.button1 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.label1 = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// button1
+			// 
+			this.button1.BackColor = System.Drawing.SystemColors.ButtonFace;
+			this.button1.Font = new System.Drawing.Font("Kristen ITC", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button1.Location = new System.Drawing.Point(12, 87);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(156, 48);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "Sales for a Category";
+			this.button1.UseVisualStyleBackColor = false;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// button2
+			// 
+			this.button2.BackColor = System.Drawing.SystemColors.ButtonFace;
+			this.button2.Font = new System.Drawing.Font("Kristen ITC", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button2.Location = new System.Drawing.Point(204, 87);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(156, 48);
+			this.button2.TabIndex = 1;
+			this.button2.Text = "Sales for a Product";
+			this.button2.UseVisualStyleBackColor = false;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Cooper Black", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.Location = new System.Drawing.Point(106, 18);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(151, 27);
+			this.label1.TabIndex = 4;
+			this.label1.Text = "View Sales ";
+			// 
+			// SalesForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+			this.ClientSize = new System.Drawing.Size(372, 160);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.button1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+			this.Name = "SalesForm";
+			this.Text = "PHP-SRePS";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Label label1;
+	}
+}
